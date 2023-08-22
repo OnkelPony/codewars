80a2fecd0b4c928a6967a44a2819702f8f7fd4fc
OnkelPony <zkurvenej@outlook.com>
2023-08-22 22:34:06 +0200

    valid braces started.

---
+++ valid_braces/kata.go
@@ -0,0 +1,16 @@
+package kata
+
+import "strings"
+
+func ValidBraces(str string) bool {
+	bracePairs = map[rune]rune{
+		'(': ')',
+		'[': ']',
+		'{': '}',
+	}
+	lastBrace = str[0]
+	for _, ch := range str[1:] {
+		if bracePairs.
+	}
+	return false
+}
