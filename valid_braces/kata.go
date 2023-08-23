6b3331ea52e4a40c3cad9aadc2570bbf9221c350
Jiří Martínek <zkurvenej@outlook.com>
2023-08-23 20:25:59 +0200

    valid braces slightly closer to solution.

--- valid_braces/kata.go
+++ valid_braces/kata.go
@@ -3,15 +3,18 @@ package kata
 import "strings"
 
 func ValidBraces(str string) bool {
-	bracePairs = map[rune]rune{
+	bracePairs := map[rune]rune{
 		'(': ')',
 		'[': ']',
 		'{': '}',
 	}
-	openingBraces = "([{"
-	var braces []rune;
+	openingBraces := "([{"
+	var braces []rune
 	for _, ch := range str {
-		if openingBraces.
+		if strings.Contains(openingBraces, string(ch)) {
+			braces = append(braces, ch)
+		} else if ch == bracePairs[braces.pop()] {
+		}
 	}
 	return false
 }
