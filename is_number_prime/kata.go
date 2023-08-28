30ffc703a260d836aedb8582fd6b10394b3fa9c1
Jiří Martínek <jmartinek@corpus.cz>
2023-08-28 17:44:01 +0200

    is_number_prime done.

--- is_number_prime/kata.go
+++ is_number_prime/kata.go
@@ -5,11 +5,11 @@ import "math"
 func IsPrime(n int) bool {
 	if n == 2 || n == 3 {
 		return true
-	} else if n%2 == 0 || n < 0 {
+	} else if n%2 == 0 || n <= 1 {
 		return false
 	}
 	upper := math.Ceil(math.Sqrt(float64(n)))
-	for i := 1; i <= int(upper); i += 2 {
+	for i := 3; i <= int(upper); i += 2 {
 		if n%i == 0 {
 			return false
 		}
