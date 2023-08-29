a14074ef172e9bd585881dce49100b03e5e45374
Jiří Martínek <jmartinek@corpus.cz>
2023-08-29 18:26:48 +0200

    maximum_subarray_sum done.

--- maximum_subarray_sum/kata.go
+++ maximum_subarray_sum/kata.go
@@ -1,5 +1,15 @@
 package kata
 
 func MaximumSubarraySum(numbers []int) int {
-
+	var max int
+	for i, _ := range numbers {
+		sum := 0
+		for j := i; j < len(numbers); j++ {
+			sum += numbers[j]
+			if sum > max {
+				max = sum
+			}
+		}
+	}
+	return max
 }
