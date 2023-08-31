52dcea34409bf3e120bcfde899f56cde3f4816f9
Jiří Martínek <jmartinek@corpus.cz>
2023-08-31 18:50:28 +0200

    whatever nearly complete.

--- Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!/kata.go
+++ Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!/kata.go
@@ -9,13 +9,13 @@ import (
 func SumDigPow(a, b uint64) []uint64 {
 	var result []uint64
 	for i := a; i <= b; i++ {
-		j := i
+		strI := strconv.FormatUint(i, 10)
 		sum := uint64(0)
-		exp := len(strconv.FormatUint(i, 10))
-		for j > 0 {
-            sum += uint64(math.Pow(float64(j % 10), float64(exp)))
-            j /= 10
-            exp--
+		exp := 1
+		for _, strDigit := range strI {
+			digit, _ := strconv.Atoi(string(strDigit))
+            sum += uint64(math.Pow(float64(digit), float64(exp)))
+            exp++
         }
 		if sum == i {
 			result = append(result, i)
