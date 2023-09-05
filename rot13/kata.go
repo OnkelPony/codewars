8d9a9fed01c6c93fc4e660bf0dd4c03d16e11246
Jiří Martínek <jmartinek@corpus.cz>
2023-09-05 19:21:52 +0200

    rot13 somehow working.

--- rot13/kata.go
+++ rot13/kata.go
@@ -1,6 +1,18 @@
 package kata
 
 func Rot13(msg string) string {
-	for _
-	return ""
+	var r rune
+	var result string
+	for _, ch := range msg {
+		if ch >= 'a' && ch <= 'm' && ch >= 'A' && ch <= 'M' {
+			r = ch + 13
+		} else if ch >= 'n' && ch <= 'z' || ch >= 'N' && ch <= 'Z' {
+			r = ch - 13
+		} else {
+			r = ch
+		}
+		result += string(r)
+
+	}
+	return result
 }
