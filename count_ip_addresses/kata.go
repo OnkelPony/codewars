d7645e245885382ad18d7676b853f9404d560687
Jiří Martínek <jmartinek@corpus.cz>
2023-09-08 18:14:53 +0200

    count_ip_addresses done.

--- count_ip_addresses/kata.go
+++ count_ip_addresses/kata.go
@@ -1,6 +1,21 @@
 package kata
 
+import (
+	"strconv"
+	"strings"
+)
+
 func IpsBetween(start, end string) int {
-    //your code goes here
-    return 0
+    startIPs := makeOctets(start)
+    endIPs := makeOctets(end)
+    return (endIPs[0] - startIPs[0]) << 24 + (endIPs[1] - startIPs[1]) << 16 + (endIPs[2] - startIPs[2]) << 8 + (endIPs[3] - startIPs[3])
+
+}
+
+func makeOctets(start string) [4]int {
+    var result [4]int
+    for i, octet := range strings.Split(start, ".") {
+        result[i], _ = strconv.Atoi(octet)
+    }
+    return result
 }
