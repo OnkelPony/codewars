379b65f4ba2fee95f742a878e5b21bc462705e1d
OnkelPony <zkurvenej@outlook.com>
2023-09-12 00:15:57 +0200

    isbn_10_validation done.

--- isbn_10_validation/kata.go
+++ isbn_10_validation/kata.go
@@ -1,5 +1,21 @@
 package kata
 
 func ValidISBN10(isbn string) bool {
-  return false // implement proper solution here
+	if len(isbn)!= 10 {
+		return false
+	}
+	var sum int
+	for i := 0; i < 9; i++ {
+		digit := int(isbn[i] - '0')
+		if digit < 0 || digit > 9 {
+			return false
+		}
+        sum += digit * (i + 1)
+    }
+	if isbn[9] == 'X' || isbn[9] == 'x' {
+		sum += 100
+	} else {
+		sum += int(isbn[9] - '0') * 10
+	}
+	return (sum % 11) == 0
 }
