a12d483fccecd6926289af499df10155b6f32232
Jiří Martínek <zkurvenej@outlook.com>
2023-09-16 23:40:08 +0200

    last_digit_large_num somehow advanced.

--- last_digit_large_num/kata.go
+++ last_digit_large_num/kata.go
@@ -1,6 +1,30 @@
 package kata
 
+import (
+	"math/big"
+)
+
 
 func LastDigit(n1, n2 string) int {
-  return 0
+  var result int
+  base := int(n1[len(n1)-1] - '0')
+  if base == 0 && len(n1) == 1 {
+    return 0
+  }
+  if base == 5 {
+    return 5
+  }
+  hugeExponent, _  := new(big.Int).SetString(n2, 10)
+  exponent := big.NewInt(0)
+  exponent.Mod(hugeExponent, big.NewInt(12))
+  result = IntPow((base), int((*exponent).Int64()))
+  return result % 10
+}
+
+func IntPow(base, exponent int) int {
+  result := 1
+  for i := 0; i < exponent; i++ {
+    result *= base
+  }
+  return result
 }
--- last_digit_large_num/kata_test.go
+++ last_digit_large_num/kata_test.go
@@ -1,27 +1,32 @@
 package kata
+
 import (
-    "strings"
-    "testing"
+	"testing"
 )
 
-// TestRot13 takes a plaintext input and its expected ciphertext output as parameters
-func TestRot13(t *testing.T) {
-    // Define a slice of structs containing test inputs and corresponding outputs
-    tests := []struct{
-        Input    string
-        ExpectedOutput string
-    } {
-        {"Hello World!", "Uryyb Jbeyq!"} ,
-        {"My name is John", "Zl anzr vf Wbua"}, 
-    }
+func TestLastDigit(t *testing.T) {
+	tests := []struct {
+		Base           string
+		Exponent       string
+		ExpectedOutput int
+	}{
+		{"4", "0", 1},
+        {"125", "12", 5},
+        {"4", "2", 6},
+        {"7", "5", 7},
+		{"4", "2", 6},
+        {"4", "1", 4},
+        {"10", "123", 0},
+		{"1606938044258990275541962092341162602522202993782792835301376", "2037035976334486086268445688409378161051468393665936250636140449354381299763336706183397376", 6},
+	}
 
-    // Loop through each struct in the tests slice
-    for _, tt := range tests {
-        actualOutput := Rot13(tt.Input)
+	// Loop through each struct in the tests slice
+	for _, tt := range tests {
+		actualOutput := LastDigit(tt.Base, tt.Exponent)
 
-        // Use the built-in strings package Compare method to compare the expected and actual outputs
-        if !strings.EqualFold(actualOutput, tt.ExpectedOutput){
-            t.Errorf("Rot13(%s): expected %s but got %s", tt.Input, tt.ExpectedOutput, actualOutput)
-        }
-    }
+		// Use the built-in strings package Compare method to compare the expected and actual outputs
+		if actualOutput != tt.ExpectedOutput {
+			t.Errorf("LastDigit(%s, %s): expected %d but got %d", tt.Base, tt.Exponent, tt.ExpectedOutput, actualOutput)
+		}
+	}
 }
