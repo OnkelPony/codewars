9d6e78d24b5c9f8ebb1d281f5d32923345bf9018
Jiří Martínek <zkurvenej@outlook.com>
2023-09-17 12:21:24 +0200

    last_digit_large_num done.

--- last_digit_large_num/kata.go
+++ last_digit_large_num/kata.go
@@ -4,27 +4,28 @@ import (
 	"math/big"
 )
 
-
 func LastDigit(n1, n2 string) int {
-  var result int
-  base := int(n1[len(n1)-1] - '0')
-  if base == 0 && len(n1) == 1 {
-    return 0
-  }
-  if base == 5 {
-    return 5
-  }
-  hugeExponent, _  := new(big.Int).SetString(n2, 10)
-  exponent := big.NewInt(0)
-  exponent.Mod(hugeExponent, big.NewInt(12))
-  result = IntPow((base), int((*exponent).Int64()))
-  return result % 10
+	var result uint
+	base := uint(n1[len(n1)-1] - '0')
+	
+	if base == 0 || base == 1 || base == 5 || base == 6 {
+		return int(base)
+	}
+	hugeExponent, _ := new(big.Int).SetString(n2, 10)
+	smallExponent := big.NewInt(0)
+	smallExponent.Mod(hugeExponent, big.NewInt(4))
+	exponent := (*smallExponent).Int64()
+	if exponent == 0 && len(n2) > 1 {
+		exponent = 4
+	}
+	result = UintPow(uint(base), uint(exponent))
+	return int(result % 10)
 }
 
-func IntPow(base, exponent int) int {
-  result := 1
-  for i := 0; i < exponent; i++ {
-    result *= base
-  }
-  return result
+func UintPow(base, exponent uint) uint {
+	result := uint(1)
+	for i := uint(0); i < exponent; i++ {
+		result *= base
+	}
+	return result
 }
