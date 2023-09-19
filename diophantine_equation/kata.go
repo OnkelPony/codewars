335ff1fd8c26869597e7f9fb40ef72adc3f5d5ef
Jiří Martínek <jmartinek@corpus.cz>
2023-09-19 18:58:31 +0200

    diophantine_equation not optimized.

--- diophantine_equation/kata.go
+++ diophantine_equation/kata.go
@@ -1,6 +1,16 @@
 package kata
 
 func Solequa(n int) [][]int {
-	var result [][]int
+	result := [][]int{}
+	for x := n / 2 + 1; x >= 0; x-- {
+		for y := x / 2 + 1; y >= 0; y-- {
+			if (x-2*y)*(x+2*y) == n {
+				result = append(result, []int{x, y})
+			}
+			if (x-2*y)*(x+2*y) > n {
+				break
+            }
+		}
+	}
 	return result
 }
--- diophantine_equation/kata_test.go
+++ diophantine_equation/kata_test.go
@@ -16,13 +16,16 @@ func TestSolequa	(t *testing.T) {
 		{9005, [][]int{{4503, 2251}, {903, 449}}},
 		{9008, [][]int{{1128, 562}}},
 		{90002, [][]int{}},
+		{1000000, [][]int{{125002, 62499}, {62504, 31248}, {31258, 15621}, {25010, 12495}, {12520, 6240}, {6290, 3105}, {5050, 2475}, {2600, 1200}, {1450, 525}, {1250, 375}, {1000, 0}}},
+        // {1000000000, [][]int{}},
+
     }
 
     for _, tt := range tests {
         actualOutput := Solequa(tt.Input)
 
         if !reflect.DeepEqual(tt.ExpectedOutput, actualOutput){
-            t.Errorf("Solequa(%d): expected %v but got %v", tt.Input, tt.ExpectedOutput, actualOutput)
+            t.Errorf("Solequa(%d): expected %v but got %#v", tt.Input, tt.ExpectedOutput, actualOutput)
         }
     }
 }
