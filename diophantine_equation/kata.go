0d5c0f39be5354eda057717d9c7b15195a32da4b
Jiří Martínek <jmartinek@corpus.cz>
2023-09-25 17:58:24 +0200

    diophantine_equation some other improvements.

--- diophantine_equation/kata.go
+++ diophantine_equation/kata.go
@@ -4,8 +4,13 @@ import "math"
 
 func Solequa(n int) [][]int {
 	result := [][]int{}
-	maxx := n/2 + 1
-	for x := maxx; x >= 0; x-- {
+	var maxx int
+	if n%2 == 0 {
+		maxx = n / 2
+	} else {
+		maxx = n/2 + 1
+	}
+	for x := maxx; x >= 0; x -= 2 {
 		y := int(math.Sqrt(float64(x*x-n) / 4))
 		if x*x-4*y*y == n {
 			result = append(result, []int{x, y})
