e16807a1d750612a14eed510534900e45bad9adb
OnkelPony <zkurvenej@outlook.com>
2023-10-03 21:21:40 +0200

    grid_travel done.

--- dynamic_prog/grid_travel.go
+++ dynamic_prog/grid_travel.go
@@ -1,11 +1,20 @@
 package dyn_prog
 
-func gridTravel(m, n int) int {
+import (
+    "fmt"
+)
+
+func gridTravel(m, n int, ways map[string]int) int {
 	if m == 1 && n == 1 {
 		return 1
 	}
 	if m == 0 || n == 0 {
 		return 0
 	}
-	return gridTravel(m - 1, n) + gridTravel(m, n - 1)
+	key := fmt.Sprintf("%d,%d", m, n)
+	if _, ok := ways[key]; ok {
+        return ways[key]
+    }
+	ways[key] = gridTravel(m - 1, n, ways) + gridTravel(m, n - 1, ways)
+	return ways[key]
 }
--- dynamic_prog/grid_travel_test.go
+++ dynamic_prog/grid_travel_test.go
@@ -13,10 +13,17 @@ func TestGridTravel(t *testing.T) {
 		{2, 3, 3},
 		{3, 3, 6},
 		{4, 5, 35},
-		// {18, 18, 2333606220},
+		{5, 5, 70},
+        {6, 6, 252},
+        {7, 7, 924},
+        {8, 8, 3432},
+        {9, 9, 12870},
+		{12, 15, 4457400},
+		{18, 18, 2333606220},
 	}
+	ways := make(map[string]int)
 	for _, c := range cases {
-		got := gridTravel(c.m, c.n)
+		got := gridTravel(c.m, c.n, ways)
 		if got != c.want {
 			t.Errorf("gridTravel(%d, %d) => %d, want %d",c.m, c.n, got, c.want)
 		}
