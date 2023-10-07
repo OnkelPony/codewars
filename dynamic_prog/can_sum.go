18f7af1be700e7c3001dd37f24516c08072885ec
OnkelPony <zkurvenej@outlook.com>
2023-10-07 17:05:01 +0200

    can_sum done.

--- dynamic_prog/can_sum.go
+++ dynamic_prog/can_sum.go
@@ -1,5 +1,25 @@
 package dyn_prog
 
-func canSum(targetSum int, numbers []int) bool {
+
+func canSum(targetSum int, numbers []int, memo map[int]bool) bool {
+	if targetSum == 0 {
+		return true
+	}
+	if targetSum < 0 {
+		return false
+	}
+	if _, ok := memo[targetSum]; ok {
+		return memo[targetSum]
+	}
+
+	for _, number := range numbers {
+		remainder := targetSum - number
+		can := canSum(remainder, numbers, memo)
+		if can {
+			memo[targetSum] = true
+			return true
+		}
+	}
+	memo[targetSum] = false
 	return false
 }
--- dynamic_prog/can_sum_test.go
+++ dynamic_prog/can_sum_test.go
@@ -6,17 +6,21 @@ import (
 
 func TestCanSum(t *testing.T) {
 	cases := []struct {
-		targetSum    int
-		numbers []int
-		want bool
+		targetSum int
+		numbers   []int
+		want      bool
 	}{
 		{8, []int{2, 3, 5}, true},
 		{8, []int{3, 6}, false},
+		{18, []int{2, 3, 5, 7}, true},
+		{108, []int{3, 5, 7}, true},
+		{3700017, []int{7, 17, 23, 29, 37}, true},
 	}
 	for _, c := range cases {
-		got := canSum(c.targetSum, c.numbers)
+		memo := make(map[int](bool))
+		got := canSum(c.targetSum, c.numbers, memo)
 		if got != c.want {
-			t.Errorf("canSum(%d, %v) => %t, want %t",c.targetSum, c.numbers, got, c.want)
+			t.Errorf("canSum(%d, %v) => %t, want %t", c.targetSum, c.numbers, got, c.want)
 		}
 	}
 }
